83105d943e56f50fc0e7249bf758662a14a89998
Yanze Liu <s3533995@student.rmit.edu.au>
2017-03-27 01:30:25 +1100

    Add some methods, and modify some code

--- src/OzlympicGame/People.java
+++ src/OzlympicGame/People.java
@@ -1,8 +1,8 @@
 package OzlympicGame;
 
-public class People {
-	private String ID;
-	private String name;
+public abstract class People {
+	protected String ID;
+	protected String name;
 	private int age;
 	private String state;
 
@@ -17,33 +17,66 @@ public class People {
 		return ID;
 	}
 	
+	public void setID(String ID) {
+		this.ID = ID;
+	}
+	
 	public String getName(){
 		return name;
+	}
+	
+	public void setName(){
+		this.name=name;
 	}
 	
 	public int getAge(){
 		return age;
 	}
 	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 	public String state(){
 		return state;
 	}
 	
-	void compete(){
-		
+	public void setState(){
+		this.state=state;
 	}
+	
 }
 
-class athletes extends People {
+abstract class athletes extends People {
+	private int points;
+	protected int time;
 	
 	public athletes(String ID, String name, int age, String state) {
 		super(ID, name, age, state);
+		this.points=0;
+		this.time=0;
 		// TODO Auto-generated constructor stub
 	}
-
-	void compele(){
-		
+	public void addPoints(int p){
+		this.points+=p;
 	}
+	
+	public int getPoints() {
+		return points;
+	}
+	
+	public void setPoints(int points) {
+		this.points = points;
+	}
+	
+	public int getTime() {
+		return time;
+	}
+	public void setTime(int time) {
+		this.time = time;
+	}
+	
+	abstract void compete(int type);
 	
 }
 
@@ -52,10 +85,18 @@ class swimmers extends athletes{
 		super(ID, name, age, state);
 		// TODO Auto-generated constructor stub
 	}
-
-	void swimming(){
+	
+	public void compete(int type) {
 		
+		 time=(int) (100+Math.random()*100);
+		 swimming();
 	}
+
+	public void swimming(){
+		System.out.println("Athlete"+ID+" "+name+" swimming  time="+time);
+	}
+	
+
 }
 
 class bicyclist extends athletes{
@@ -63,10 +104,16 @@ class bicyclist extends athletes{
 		super(ID, name, age, state);
 		// TODO Auto-generated constructor stub
 	}
-
-	void cycling(){
+	public void compete(int type) {
 		
+		 time=(int) (500+Math.random()*300);
+		 cycly();
 	}
+	
+   public void cycly(){
+   	System.out.println("Athlete"+ID+" "+name+" cycling  time="+time);
+   }
+   
 }
 
 class runner extends athletes{
@@ -74,10 +121,15 @@ class runner extends athletes{
 		super(ID, name, age, state);
 		// TODO Auto-generated constructor stub
 	}
-
-	void running(){
+	public void compete(int type) {
 		
+		time=(int) (10+Math.random()*10);
+		running();
 	}
+   public void running(){
+   	  System.out.println("Athlete"+ID+" "+name+" running  time="+time);
+   }
+
 }
 
 class superAthletes extends athletes{
@@ -85,14 +137,34 @@ class superAthletes extends athletes{
 		super(ID, name, age, state);
 		// TODO Auto-generated constructor stub
 	}
+	
+	public void compete(int type) {
+		if(type==1){
+			
+			time=(int) (10+Math.random()*10);
+			running();
+		}
+		else if(type==2){
+			
+			time=(int) (100+Math.random()*100);
+			swimming();
+		}
+		else if(type==3){
+			
+			time=(int) (500+Math.random()*300);
+			cycling();
+		}
+	}
 	void swimming(){
-		
+		System.out.println("Athlete"+ID+" "+name+" swimming  time="+time);
 	}
+	
 	void cycling(){
-		
+		System.out.println("Athlete"+ID+" "+name+" cycling  time="+time);
 	}
+	
 	void running(){
-		
+		System.out.println("Athlete"+ID+" "+name+" running  time="+time);
 	}
 }
 
