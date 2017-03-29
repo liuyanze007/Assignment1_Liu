6b7398155257951f34e2d4e88bb4fd02b337d1e2
Yanze Liu <s3533995@student.rmit.edu.au>
2017-03-30 01:58:49 +1100

    code the main selection

--- src/OzlympicGame/OzlympicGameSystem.java
+++ src/OzlympicGame/OzlympicGameSystem.java
@@ -1,6 +1,28 @@
 package OzlympicGame;
 
+import java.util.List;
+import java.util.Scanner;
 
 public class OzlympicGameSystem {
+	private List<Game> games;//all games
+	private List<Athletes> allAthletes;//all athletes
+	private Scanner sc;
+	private int whichGame;//the game selected
+	private User user; //the user that predict the game
 
+	
+	
+	//print the menu
+	public void printMenu(){
+		System.out.println("Ozlympic	Game");
+		System.out.println("===================================");
+		System.out.println("1.	Select a game to run");
+		System.out.println("2.	Predict the winner of the game");
+		System.out.println("3.	Start the game");	
+		System.out.println("4.	Display the final results of all games");
+		System.out.println("5.	Display the points of all athletes");
+		System.out.println("6.	Exit");
+		System.out.println("");
+		System.out.print("Enter an option:");
+	}
 }
