9aac1606826e0d7f57cc4848f0d92345161d2ad8
Yanze Liu <s3533995@student.rmit.edu.au>
2017-04-02 14:05:42 +1000

    add the main method

--- src/OzlympicGame/OzlympicGameSystem.java
+++ src/OzlympicGame/OzlympicGameSystem.java
@@ -1,5 +1,6 @@
 package OzlympicGame;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -10,7 +11,13 @@ public class OzlympicGameSystem {
 	private int whichGame;//the game selected
 	private User user; //the user that predict the game
 
-	
+	public void Ozlympic(Scanner sc){
+		games=new ArrayList<Game>();
+		allAthletes=new ArrayList<Athletes>();
+		this.sc=sc;
+		whichGame=-1;
+		user=new User();
+	}
 	
 	//print the menu
 	public void printMenu(){
@@ -24,5 +31,74 @@ public class OzlympicGameSystem {
 		System.out.println("6.	Exit");
 		System.out.println("");
 		System.out.print("Enter an option:");
+	}
+	
+	public OzlympicGameSystem(Scanner sc){
+		games=new ArrayList<Game>();
+		allAthletes=new ArrayList<Athletes>();
+		this.sc=sc;
+		whichGame=-1;
+		user=new User();
+		
+	}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		OzlympicGameSystem op=new OzlympicGameSystem(sc);
+		op.readFile("input.txt");
+		int cmd=0;
+		while(true){
+			op.printMenu();
+			cmd=sc.nextInt();
+			if(cmd==1){
+				op.selectGame();
+			}
+			else if(cmd==2){
+				op.PredictGame();
+			}
+            else if(cmd==3){
+				op.startGame();
+			}
+            else if(cmd==4){
+				op.displayGameResult();
+			}
+            else if(cmd==5){
+				op.displayAllAthlete();
+			}
+			else if(cmd==6){
+				return;
+			}
+		}
+		
+}
+
+	private void displayAllAthlete() {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+	private void displayGameResult() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void startGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void PredictGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void selectGame() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void readFile(String string) {
+		// TODO Auto-generated method stub
+		
 	}
 }
